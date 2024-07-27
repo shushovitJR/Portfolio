bd681dc32da9d7b5d7557de9b30a3e63b002afe6
shushovitJR <ranashushovit@gmail.com>
2024-07-27 22:43:49 +0545

    Added jquery,ajax

--- js/api2.js
+++ js/api2.js
@@ -1,47 +1,34 @@
-let text = document.querySelector("input");
-let btn = document.getElementById("btn");
-let weather = document.getElementById("weather");
-let loc = document.getElementById("location");
-let speed = document.getElementById("speed");
-let temp = document.getElementById("temp");
-let minmax = document.getElementById("minmax");
-let time = document.getElementById("time");
-let body = document.querySelector("body");
-start();
-function start() {
-  getweather("kathmandu");
-}
-btn.addEventListener("click", (e) => {
-  e.preventDefault();
-  let input = text.value;
-  getweather(input);
-  text.value = "";
-});
-function getweather(input) {
-  fetch(
-    `https://api.openweathermap.org/data/2.5/weather?q=${input}&appid=2ecb730b54cc0e1000d571b42c1733b9`
-  )
-    .then((result) => {
-      return result.json();
-    })
-    .then((data) => {
-      let txt = data.weather[0].description;
-      weather.innerHTML = txt;
-      txt = data.sys.country;
-      loc.innerHTML = input.toUpperCase() + "," + txt;
-      txt = data.wind.speed;
-      speed.innerHTML = txt + " km/hr ";
-      txt = parseInt(data.main.temp - 273);
-      temp.innerHTML = txt + "°C";
-      txt = parseInt(data.main.temp_min - 273);
-      let txt2 = parseInt(data.main.temp_max - 273);
-      minmax.innerHTML = txt + "°C(min) | " + txt2 + "°C(max)";
+function getWeather(input) {
+  $.ajax({
+    url: `https://api.openweathermap.org/data/2.5/weather?q=${input}&appid=2ecb730b54cc0e1000d571b42c1733b9`,
+    type: 'GET',
+    dataType: 'json',
+    success: function(data) {
+      // Extract and display the weather information
+      let weatherDescription = data.weather[0].description;
+      $('#weather').html(weatherDescription);
 
-      const str = weather.innerHTML;
-      console.log(str);
+      let country = data.sys.country;
+      $('#loc').html(input.toUpperCase() + "," + country);
 
-    })
-    .catch((err) => {
+      let windSpeed = data.wind.speed;
+      $('#speed').html(windSpeed + " km/hr");
+
+      let tempCelsius = parseInt(data.main.temp - 273);
+      $('#temp').html(tempCelsius + "°C");
+
+      let tempMinCelsius = parseInt(data.main.temp_min - 273);
+      let tempMaxCelsius = parseInt(data.main.temp_max - 273);
+      $('#minmax').html(tempMinCelsius + "°C(min) | " + tempMaxCelsius + "°C(max)");
+
+      // Log weather description to the console
+      console.log(weatherDescription);
+    },
+    error: function(err) {
       console.log(err.message);
-    });
+    }
+  });
 }
+$(document).ready(function() {
+  getWeather('Kathmandu');
+});
